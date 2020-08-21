72fb3f84af47f617da850a0381593534589fd4f8
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-08-21 13:30:41 +0100

    Add pages for proxy poc

---
+++ proxy.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('paymentForm');
+const formBtn = document.getElementById('formBtn');
+const inputs = document.getElementById('inputs');
+
+function addInput(name, value) {
+    let input = document.createElement('input');
+    input.setAttribute('name', name);
+    input.setAttribute('value', value);
+    inputs.appendChild(input);
+
+}
+
+function submitForm(e) {
+    e.preventDefault();
+    form.action = document.getElementById('instance').value
+    addInput("CallingApplicationID", "Outsystems");
+    addInput("FundCode", "42");
+    addInput("PaymentSourceCode", "01");
+    addInput("CallingApplicationTransactionReference", `ProxyTest_${Date.now()}`);
+    addInput("Payment_1", "BLB-J6300-0110000929|42|10.00|SE|Blue Badge||||||||||||");
+    addInput("PaymentTotal", "10");
+    addInput("ReturnURL", "https://www.google.com");
+    addInput("BackButtonURL", "https://www.google.com");
+    form.submit();
+}
+
+formBtn.addEventListener('click', submitForm);
